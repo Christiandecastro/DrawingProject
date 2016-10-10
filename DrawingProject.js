aaac4b303999897b5d99c0b4c78253afca42bd9e
christian de castro <robert12102000@yahoo.com>
2016-10-10 16:56:38 -0700

    made a js file
    created a js file

---
+++ DrawingProject.js
@@ -0,0 +1,173 @@
+var canvasDiv = document.getElementById("drawingArea");
+var canvas = canvasDiv.getContext('2d');
+
+canvas.fillStyle = "blue";
+canvas.fillRect(0,0,2000,1000);
+
+canvas.fillStyle = "black";
+canvas.fillRect(700,300,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(700,320,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(700,280,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(720,260,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(720,240,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(740,220,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(760,200,60,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(800,180,60,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(860,200,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(880,220,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(880,240,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(900,240,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(860,240,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(840,240,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(840,260,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(820,220,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(860,280,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(880,280,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(920,260,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(920,280,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(920,300,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(920,320,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(920,340,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(920,360,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(920,380,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(920,400,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(940,400,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(900,400,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(880,420,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(880,420,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(860,440,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(840,440,20,20);
+
+
+canvas.fillStyle = "black";
+canvas.fillRect(820,440,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(800,440,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(780,440,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(760,420,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(740,400,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(720,380,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(720,360,20,20);
+
+
+canvas.fillStyle = "black";
+canvas.fillRect(720,340,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(860,400,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(840,400,20,20);
+
+
+canvas.fillStyle = "black";
+canvas.fillRect(820,400,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(800,400,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(840,320,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(840,340,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(820,340,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(820,320,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(880,320,20,20);
+
+canvas.fillStyle = "black";
+canvas.fillRect(880,340,20,20);
+
+canvas.fillStyle = "white";
+canvas.fillRect(900,300,20,80);
+
+canvas.fillStyle = "white";
+canvas.fillRect(880,360,20,20);
+
+canvas.fillStyle = "white";
+canvas.fillRect(800,360,60,20);
+
+canvas.fillStyle = "white";
+canvas.fillRect(800,300,60,20);
+
+canvas.fillStyle = "white";
+canvas.fillRect(780,320,40,40);
